8633b838c4fa6cb5d31189e426d9e1ff2eaf85d9
youssefhesham2000 <youssefhesham2000@yahoo.com>
2020-11-25 04:37:36 +0200

    FIRST

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,58 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+   <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+    integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
+    crossorigin="anonymous"
+  />
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" >Paint</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+       <button class="btn btn-outline-success" type="button">Save</button>
+    <button class="btn btn-outline-info" type="button">Load</button>
+    </div>
   </div>
+</nav>
+  <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+  <div class="btn-group btn-group-lg" role="group" aria-label="First group">
+    <button type="button" class="btn btn-secondary">Line</button>
+    <button type="button" class="btn btn-secondary">circle</button>
+    <button type="button" class="btn btn-secondary">Ellipse</button>
+    <button type="button" class="btn btn-secondary">Triangle</button>
+    <button type="button" class="btn btn-secondary">Rectangle</button>
+    <button type="button" class="btn btn-secondary">Square</button>
+  </div>
+  <div class="btn-group btn-group-lg" role="group" aria-label="Second group">
+    <button type="button" class="btn btn-danger">Undo</button>
+    <button type="button" class="btn btn-success">Redo</button>
+  </div>
+</div>
+<canvas id="canvas" height="959" width=1500></canvas>
+
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  
 }
 </script>
 
+
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+div[aria-label="First group"]{
+ width : 100%; 
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+div[aria-label="Second group"]{
+ width : 35%;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+#canvas {
+    margin-top: 50px;
+    border: 3px solid black;
+  width: 100%;
 }
 </style>
